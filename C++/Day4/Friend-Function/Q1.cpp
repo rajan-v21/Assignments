a09751d51f895dd0dfb9e1487abcc244b0b788b5
Rajan Vishwakarma <rajanvishwakarma218@gmail.com>
2024-05-12 20:18:20 +0530

    Day4 friend function assignment

---
+++ C++/Day4/Friend-Function/Q1.cpp
@@ -0,0 +1,16 @@
+// 1) class First
+//    {
+//       char str1[20];
+//    };
+//    class Second
+//    {
+//       char str2[20];
+//    };
+// write a friend function,which will create a  new string containing str1,str2 and also display it.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class First{
+    char str1[20];
+};
