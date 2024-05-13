b47bc1e01d5cab437fff86c558d9287f93ba6ad2
Rajan Vishwakarma <rajanvishwakarma218@gmail.com>
2024-05-13 21:06:18 +0530

    Day4 operator overloading Q2

---
+++ C++/Day4/Operator-Overloading/Q2.cpp
@@ -0,0 +1,22 @@
+// 2) class sample
+//    {
+// 	private:
+// 		char ch;
+// 	public:
+// 		sample(char ch)
+// 		{
+// 			this->ch=ch;
+// 		}
+//   };
+
+// given above class.
+
+// write a global function "display" which will access "ch" directly and display it.
+// call this function from main function.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class sample{
+
+};
